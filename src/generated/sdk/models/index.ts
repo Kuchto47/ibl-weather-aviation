e857a11b60875d19b9a692070d02907f575459bf
Kuchto47 <martin.k738@gmail.com>
2024-01-31 14:12:13 +0100

    generate api sdk

---
+++ src/generated/sdk/models/index.ts
@@ -0,0 +1,6 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './OpmetRequest';
+export * from './OpmetRequestParam';
+export * from './OpmetResponse';
+export * from './OpmetResponseResult';
