1f29d53de9f8984411dde198469988ddf8085d01
Kuchto47 <martin.k738@gmail.com>
2024-02-01 00:12:35 +0100

    refactor App component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,14 +1,12 @@
-import { useState } from 'react'
-import reactLogo from '../assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { Header } from './common/components/Header'
-import { OptionsPanel } from './common/components/OptionsPanel'
+import { useState } from 'react';
+
+import './App.css';
+import { Header } from './common/components/Header';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline'
+import CssBaseline from '@mui/material/CssBaseline';
+import { Content } from './common/components/Content';
 
 export const App = () => {
-  const [count, setCount] = useState(0);
   const [isDarkMode, setIsDarkMode] = useState(true);
 
   const toggleDarkMode = () => {
@@ -30,28 +28,8 @@ export const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <Header />
-      <OptionsPanel darkMode={isDarkMode} toggleDarkMode={toggleDarkMode}/>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Header darkMode={isDarkMode} toggleDarkMode={toggleDarkMode}/>
+      <Content />
     </ThemeProvider>
   );
 }
