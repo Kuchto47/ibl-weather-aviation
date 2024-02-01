844862ac0f080cd449b363742afb9628468c48e2
Kuchto47 <martin.k738@gmail.com>
2024-02-01 23:50:09 +0100

    make checkboxes with labels more pleasing

--- src/app/weather_briefing/components/Checkboxes.tsx
+++ src/app/weather_briefing/components/Checkboxes.tsx
@@ -1,10 +1,24 @@
-import { Checkbox, FormControl, FormControlLabel, FormGroup, FormHelperText, FormLabel } from "@mui/material";
-import { useState } from "react";
+import {
+    Checkbox,
+    FormControl,
+    FormControlLabel,
+    FormGroup
+} from '@mui/material';
+import { useState } from 'react';
+import { createUseStyles } from 'react-jss';
+
+const useStyles = createUseStyles({
+    checkbox: {
+        marginRight: '-7px'
+    }
+});
 
 export const Checkboxes = () => {
     const [metarChecked, setMetarChecked] = useState(false);
     const [sigmetChecked, setSigmetChecked] = useState(false);
     const [tafChecked, setTafChecked] = useState(false);
+
+    const styles = useStyles();
 
     const toggleMetar = (event: React.ChangeEvent<HTMLInputElement>) => {
         setMetarChecked(event.target.checked);
@@ -17,7 +31,7 @@ export const Checkboxes = () => {
     const toggleTaf = (event: React.ChangeEvent<HTMLInputElement>) => {
         setTafChecked(event.target.checked);
     };
-    
+
     const isErroredCheckboxes =
         [metarChecked, sigmetChecked, tafChecked].filter((rt) => !!rt).length === 0;
 
@@ -30,6 +44,7 @@ export const Checkboxes = () => {
                             checked={metarChecked}
                             onChange={toggleMetar}
                             size="small"
+                            className={styles.checkbox}
                         />
                     }
                     label="METAR"
@@ -40,17 +55,23 @@ export const Checkboxes = () => {
                             checked={sigmetChecked}
                             onChange={toggleSigmet}
                             size="small"
+                            className={styles.checkbox}
                         />
                     }
                     label="SIGMET"
                 />
                 <FormControlLabel
                     control={
-                        <Checkbox checked={tafChecked} onChange={toggleTaf} size="small" />
+                        <Checkbox
+                            checked={tafChecked}
+                            onChange={toggleTaf}
+                            size="small"
+                            className={styles.checkbox}
+                        />
                     }
                     label="TAF"
                 />
             </FormGroup>
         </FormControl>
     );
-}
+};
