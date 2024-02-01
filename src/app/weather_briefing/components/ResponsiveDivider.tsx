f0553b5ac0086771caef635b824906f1c0db21e0
Kuchto47 <martin.k738@gmail.com>
2024-02-01 20:33:22 +0100

    add responsive divider

---
+++ src/app/weather_briefing/components/ResponsiveDivider.tsx
@@ -0,0 +1,9 @@
+import { Theme } from '@mui/material';
+import Divider from '@mui/material/Divider';
+import useMediaQuery from '@mui/material/useMediaQuery';
+
+export const ResponsiveDivider = () => {
+    const isLgScreen = useMediaQuery<Theme>((theme) => theme.breakpoints.down('lg'));
+
+    return <Divider orientation={isLgScreen ? 'horizontal' : 'vertical'} />;
+};
