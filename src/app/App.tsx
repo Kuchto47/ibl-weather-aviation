59bb164bd9687c36ca9af0ac29b7e1cfc95e140e
Kuchto47 <martin.k738@gmail.com>
2024-02-01 14:05:45 +0100

    format code

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,29 +5,29 @@ import CssBaseline from '@mui/material/CssBaseline';
 import { Content } from './common/components/Content';
 
 export const App = () => {
-  const [isDarkMode, setIsDarkMode] = useState(true);
+    const [isDarkMode, setIsDarkMode] = useState(true);
 
-  const toggleDarkMode = () => {
-    setIsDarkMode(currentTheme => !currentTheme);
-  }
+    const toggleDarkMode = () => {
+        setIsDarkMode((currentTheme) => !currentTheme);
+    };
 
-  const theme = createTheme({
-    palette: {
-      mode: isDarkMode ? 'dark' : 'light',
-      primary: {
-        main: '#90caf9',
-      },
-      secondary: {
-        main: '#131052',
-      },
-    },
-  });
+    const theme = createTheme({
+        palette: {
+            mode: isDarkMode ? 'dark' : 'light',
+            primary: {
+                main: '#90caf9'
+            },
+            secondary: {
+                main: '#131052'
+            }
+        }
+    });
 
-  return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Header darkMode={isDarkMode} toggleDarkMode={toggleDarkMode}/>
-      <Content />
-    </ThemeProvider>
-  );
-}
+    return (
+        <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <Header darkMode={isDarkMode} toggleDarkMode={toggleDarkMode} />
+            <Content />
+        </ThemeProvider>
+    );
+};
