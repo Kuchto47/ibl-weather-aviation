15459a24ed87bed15c52b5b5f76d4da19a50bec0
Kuchto47 <martin.k738@gmail.com>
2024-02-01 20:33:49 +0100

    add form backbone

--- src/app/weather_briefing/components/Form.tsx
+++ src/app/weather_briefing/components/Form.tsx
@@ -1,13 +1,28 @@
-import { PropsWithChildren } from 'react';
+import { PropsWithChildren, useState } from 'react';
 import { useFetchWeatherData } from '../hooks/useFetchWeatherData';
 import { WeatherQuery } from '../model/WeatherQuery';
 import { BriefingDataArray } from '../model/BriefingData';
+import { OpmetRequestParamDTOReportTypesEnum } from '../../../generated/sdk/models';
+
+type ReportType = 0 | 1 | 2;
 
 interface Props {
     onBriefingReceived: (briefing: BriefingDataArray) => void;
 }
 
+interface FormData {
+    reportTypes: OpmetRequestParamDTOReportTypesEnum[];
+    airports: string;
+    countries: string;
+}
+
 export const Form = (props: PropsWithChildren<Props>) => {
+    const [formData, setFormData] = useState<FormData>({
+        reportTypes: [],
+        airports: '',
+        countries: ''
+    });
+    
     const fetchData = useFetchWeatherData();
 
     const getWeatherBriefing = async () => {
