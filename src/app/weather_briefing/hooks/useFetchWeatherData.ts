ccf080b01fed80eb142918cf99f07a9d4c594f52
Kuchto47 <martin.k738@gmail.com>
2024-02-01 17:01:13 +0100

    implement useFetchWeather hook

--- src/app/weather_briefing/hooks/useFetchWeatherData.ts
+++ src/app/weather_briefing/hooks/useFetchWeatherData.ts
@@ -1,8 +1,14 @@
-import { useCallback } from "react";
+import { useCallback } from 'react';
+import weatherBriefingService from '../services/weatherBriefingService';
+import { WeatherQuery } from '../model/WeatherQuery';
+import { mapResponseDtoToBriefingData } from '../services/mapperService';
+import { BriefingDataArray } from '../model/BriefingData';
 
 export const useFetchWeatherData = () => {
-
-    return useCallback(async () => {
-        
+    return useCallback(async (query: WeatherQuery): Promise<BriefingDataArray> => {
+        const result = await weatherBriefingService.getWeatherBriefing(query);
+        return result
+            ? mapResponseDtoToBriefingData(result)
+            : [];
     }, []);
 };
