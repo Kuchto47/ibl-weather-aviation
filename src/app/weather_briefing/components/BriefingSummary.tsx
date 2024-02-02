d3ef21666b563eafd300b6df9a05162d185f28d2
Kuchto47 <martin.k738@gmail.com>
2024-02-02 20:28:39 +0100

    render response

--- src/app/weather_briefing/components/BriefingSummary.tsx
+++ src/app/weather_briefing/components/BriefingSummary.tsx
@@ -1,8 +1,44 @@
 import { useContext } from 'react';
 import { BriefingContext } from '../contexts/BriefingContext';
+import { Paper, Table, TableBody, TableCell, TableContainer, TableRow } from '@mui/material';
+import { BriefingData } from '../model/BriefingData';
+import { formatDate } from '../utils/formatDate';
 
 export const BriefingSummary = () => {
     const { briefingData } = useContext(BriefingContext);
 
-    return <p>{briefingData.response ? JSON.stringify(briefingData.response) : '[]'}</p>;
+    return (
+        <>
+            <Paper sx={{ width: '100%', overflow: 'hidden' }}>
+                <TableContainer sx={{ maxHeight: '440px' }}>
+                    <Table sx={{ minWidth: '475px' }}>
+                        <TableBody>
+                            {briefingData.response ? (
+                                briefingData.response.map(createTableRecord)
+                            ) : (
+                                <></>
+                            )}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            </Paper>
+        </>
+    );
+};
+
+const createTableRecord = (data: BriefingData, index: number) => {
+    return (
+        <>
+            <TableRow>
+                <TableCell>{data.stationId}</TableCell>
+                <TableCell />
+                <TableCell />
+            </TableRow>
+            <TableRow>
+                <TableCell>{data.queryType}</TableCell>
+                <TableCell>{formatDate(data.reportTime)}</TableCell>
+                <TableCell>{data.text}</TableCell>
+            </TableRow>
+        </>
+    );
 };
