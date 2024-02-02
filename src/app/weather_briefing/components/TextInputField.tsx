43b642f51a169f6b3bc572491248ae37d1901918
Kuchto47 <martin.k738@gmail.com>
2024-02-02 19:03:57 +0100

    add context updating in text input fields

--- src/app/weather_briefing/components/TextInputField.tsx
+++ src/app/weather_briefing/components/TextInputField.tsx
@@ -1,14 +1,31 @@
 import { FormControl, TextField } from '@mui/material';
-import { PropsWithChildren } from 'react';
+import { ChangeEvent, PropsWithChildren, useContext, useState } from 'react';
+import { BriefingContext } from '../contexts/BriefingContext';
 
 interface Props {
     label: 'Airports' | 'Countries';
 }
 
 export const TextInputField = (props: PropsWithChildren<Props>) => {
+    const { briefingData, updateContext } = useContext(BriefingContext);
+    const [value, setValue] = useState('');
+
+    const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+        setValue(event.currentTarget.value);
+        updateContext({
+            ...briefingData,
+            query: {
+                ...briefingData.query,
+                ...(props.label === 'Airports'
+                    ? { stations: event.currentTarget.value }
+                    : { countries: event.currentTarget.value })
+            }
+        });
+    };
+
     return (
         <FormControl fullWidth>
-            <TextField label={props.label} variant="filled" />
+            <TextField label={props.label} variant="filled" value={value} onChange={handleChange} />
         </FormControl>
     );
 };
