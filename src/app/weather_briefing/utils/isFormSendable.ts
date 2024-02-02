eccdcb16b9a93b9072b4e426dd58f9f03cee549f
Kuchto47 <martin.k738@gmail.com>
2024-02-02 19:38:51 +0100

    improve checking of sendability of query

--- src/app/weather_briefing/utils/isFormSendable.ts
+++ src/app/weather_briefing/utils/isFormSendable.ts
@@ -1,11 +1,29 @@
 import { BriefingContextData } from '../contexts/BriefingContext';
+import { ReportTypes, WeatherQuery } from '../model/WeatherQuery';
 
-export const isFormSendable = (briefingData: BriefingContextData) => {
+export const isFormSendable = (briefingData: BriefingContextData): boolean => {
     return (
-        [
-            briefingData.query.reportTypes.METAR,
-            briefingData.query.reportTypes.SIGMET,
-            briefingData.query.reportTypes.TAF_LONGTAF
-        ].filter((rt) => !!rt).length > 0
+        atLeastOneCheckboxActive(briefingData.query.reportTypes) &&
+        metarAirportNonEmpty(briefingData.query) &&
+        countriesNonEmpty(briefingData.query)
     );
 };
+
+const atLeastOneCheckboxActive = (reportTypes: ReportTypes): boolean => {
+    return (
+        [reportTypes.METAR, reportTypes.SIGMET, reportTypes.TAF_LONGTAF].filter((rt) => !!rt)
+            .length > 0
+    );
+};
+
+const metarAirportNonEmpty = (query: WeatherQuery): boolean => {
+    if (!query.reportTypes.METAR) return true;
+    return query.stations ? query.stations.length >= 4 : false;
+};
+
+const countriesNonEmpty = (query: WeatherQuery): boolean => {
+    if (!query.reportTypes.SIGMET && !query.reportTypes.TAF_LONGTAF) return true;
+    return query.countries ? query.countries.length >= 2 : false;
+};
+
+// TODO this could be done better, eg. airports and countries could be checked by yup or zod for schema fulfillment and upon error make it unsendable...
