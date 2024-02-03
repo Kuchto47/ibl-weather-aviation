f27ac22cdcf7fdfcd39fa2c8bbaef18f0f1c543b
Kuchto47 <martin.k738@gmail.com>
2024-02-03 12:46:57 +0100

    improve form sendability detection

--- src/app/weather_briefing/utils/isFormSendable.ts
+++ src/app/weather_briefing/utils/isFormSendable.ts
@@ -4,7 +4,7 @@ import { ReportTypes, WeatherQuery } from '../model/WeatherQuery';
 export const isFormSendable = (briefingData: BriefingContextData): boolean => {
     return (
         atLeastOneCheckboxActive(briefingData.query.reportTypes) &&
-        metarAirportNonEmpty(briefingData.query) &&
+        metarAirportOrCountryNonEmpty(briefingData.query) &&
         countriesNonEmpty(briefingData.query)
     );
 };
@@ -16,14 +16,42 @@ const atLeastOneCheckboxActive = (reportTypes: ReportTypes): boolean => {
     );
 };
 
-const metarAirportNonEmpty = (query: WeatherQuery): boolean => {
+const metarAirportOrCountryNonEmpty = (query: WeatherQuery): boolean => {
     if (!query.reportTypes.METAR) return true;
-    return query.stations ? query.stations.length >= 4 : false;
+    return metarMinimumRequirementsMet(query);
 };
 
 const countriesNonEmpty = (query: WeatherQuery): boolean => {
     if (!query.reportTypes.SIGMET && !query.reportTypes.TAF_LONGTAF) return true;
-    return query.countries ? query.countries.length >= 2 : false;
+    return countriesLengthSufficient(query.countries);
 };
 
-// TODO this could be done better, eg. airports and countries could be checked by yup or zod for schema fulfillment and upon error make it unsendable...
+const airportsLengthSufficient = (airports: string | undefined): boolean => {
+    return !!(airports && airports.length >= 4); // TODO regex test
+};
+
+const airportsEmpty = (airports: string | undefined): boolean => {
+    return !airports || airports.length === 0;
+};
+
+const countriesLengthSufficient = (countries: string | undefined): boolean => {
+    return !!(countries && countries.length >= 2); // TODO regex test
+};
+
+const countriesEmpty = (countries: string | undefined): boolean => {
+    return !countries || countries.length === 0;
+};
+
+const bothCountriesAndAirportsAreEmpty = (query: WeatherQuery) => {
+    return countriesEmpty(query.countries) && airportsEmpty(query.stations);
+};
+
+const metarMinimumRequirementsMet = (query: WeatherQuery) => {
+    return (
+        !bothCountriesAndAirportsAreEmpty(query) &&
+        (airportsLengthSufficient(query.stations) || airportsEmpty(query.stations)) &&
+        (countriesLengthSufficient(query.countries) || countriesEmpty(query.countries))
+    );
+};
+
+// TODO this could be done better, eg. airports and countries could be checked by zod for schema fulfillment and upon error make it unsendable...
