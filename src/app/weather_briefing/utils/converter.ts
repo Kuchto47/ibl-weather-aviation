25136483e66082ee0021eef94724afd33e7122fd
Kuchto47 <martin.k738@gmail.com>
2024-02-04 18:02:01 +0100

    move converter to common

--- src/app/weather_briefing/utils/converter.ts
+++
@@ -1,9 +0,0 @@
-type ObjectifiedMapRecord<K, V> = {
-    key: K;
-    value: V;
-};
-
-export function convertMapToArray<K, V>(map: Map<K, V> | undefined): ObjectifiedMapRecord<K, V>[] {
-    if (!map) return [];
-    return Array.from(map, ([key, value]) => ({ key, value }));
-}
