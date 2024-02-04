603544424249a0f991d5d9fc24593bae9199388f
Kuchto47 <martin.k738@gmail.com>
2024-02-04 18:53:09 +0100

    refactor fn name

--- src/app/weather_briefing/utils/decorateBriefingMessage.ts
+++ src/app/weather_briefing/utils/decorateBriefingMessage.ts
@@ -17,12 +17,12 @@ export class BriefingMessageDecorator {
         const regex = new RegExp(`\\b(${cloudSeverity}[0-9]{3})\\b`, 'g');
         this.messageToDecorate = this.messageToDecorate.replace(regex, (match) => {
             const number = Number(match.slice(-3));
-            return `<font color=\"${this.shouldBeBlue(number) ? this.adaptBlueColor() : 'red'}\">${match}</font>`;
+            return `<font color=\"${this.shouldBeBlue(number) ? this.themeSpecificBlue() : 'red'}\">${match}</font>`;
         });
         return this;
     }
 
-    private adaptBlueColor(): string {
+    private themeSpecificBlue(): string {
         return this.palette.mode === 'dark' ? this.palette.primary.main : 'blue';
     }
 
